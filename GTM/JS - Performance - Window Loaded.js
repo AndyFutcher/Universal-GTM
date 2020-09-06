b57a7f1fdae8abf72c78b85b52ebda2b7beb1206
SuccessEvents <13653414+SuccessEvents@users.noreply.github.com>
2020-09-06 08:20:05 +0800

    Create JS - Performance - Window Loaded.js
    Passes Page Dependencies To GA

---
+++ GTM/JS - Performance - Window Loaded.js
@@ -0,0 +1,20 @@
+<script>
+// Pass Page Dependencies To GA
+if (window.performance) { // Performance Supported
+  var resources=performance.getEntriesByType("resource"),resourceVar='',resourceValue=0,resourceLabel={{Page Navigation}};
+  for (var resource=0;resource<resources.length;resource++) {
+    resourceVar=resources[resource].name.split(';')[0]; // Strip Code/BASE64
+    resourceVar=resourceVar.split('?')[0]; // Strip Parameters
+    resourceValue=resources[resource].responseEnd-resources[resource].startTime;
+    if (resourceValue>0) { // Not Cached (Dont Round)
+      dataLayer.push({
+         'event':'timing'
+        ,'EventCategory':resources[resource].initiatorType
+        ,'EventVar':resourceVar
+        ,'EventValue':parseInt(resourceValue)
+        ,'EventLabel':resourceLabel
+      });
+    }
+  }
+}
+</script>
