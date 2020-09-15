275d9c4f600897ae0ac3d0713e0016a414a57229
SuccessEvents <13653414+SuccessEvents@users.noreply.github.com>
2020-09-15 12:37:46 +0800

    Update CJS - Client GID.js

--- GTM/CJS - Client GID.js
+++ GTM/CJS - Client GID.js
@@ -4,18 +4,23 @@ function(){ // Get Client GID (24hr clientId)
 		,get:"_gid"
 		,name:"_gid"	// cookieName="_gid"
 		,from:"{{LT - Tracking ID}}"	// TrackingId
+		,gid:""	// Placeholder
 	};
 
 	// Find Google Analytics Objects
 	if(typeof ga==="function"){	// GA Is Declared
 		var _GAobj=ga.getAll();	// Load Tracker Objects
 		if(_GA['env']==-1){	// Find Matching Tracker
-			for (i=0;i<=(_GAobj.length-1);i++){	// Find Matching TrackingId
+			var _GAlen=_GAobj.length-1;	// Tracker Count
+			for (i=0;i<=_GAlen;i++){	// Find TrackingId
 				if(_GAobj[i].get('trackingId')==_GA['from']){_GA['env']=i;break;};
 			}
 		}; // Returning Valid GA Reference
 		if(_GA['env']>-1){	// Has Valid Tracking Id
-			return _GAobj[_GA['env']].get(_GA['get']); // Get Client Id
+			_GA['gid']=_GAobj[_GA['env']].get(_GA['get']); // Get Client Id
 		}
 	}
+
+	// Return (last)
+	return _GA['gid'];
 }
